5731a6b2d63cc294ad76b1afe5925bfd4b04131e
AquaticOcellot <aquaticocellot@gmail.com>
2024-11-25 12:10:06 +0300

    normalize values to [0, 1]

--- src/perlin.ts
+++ src/perlin.ts
@@ -1,6 +1,7 @@
 import {createNoise2D} from "simplex-noise"
 
 const noise = createNoise2D()
+const amplitudeDelta = 1.15
 
 export const Perlin = (
     dimensions: number[],
@@ -11,20 +12,27 @@ export const Perlin = (
     for (let layer_index = 0; layer_index < layer_number; layer_index++) {
         layer_offsets.push([Math.random() * 1000, Math.random() * 1000])
     }
+    let max_value = 0
     for (let row_index = 0; row_index < dimensions[1]; row_index++) {
         const row: number[] = []
         for (let col_index = 0; col_index < dimensions[0]; col_index++) {
             let value = 0
             for (let layer_index = 0; layer_index < layer_number; layer_index++) {
-                value = value + noise(
+                value += amplitudeDelta ** layer_index * (noise(
                     col_index / 400 * 2 ** layer_index + layer_offsets[layer_index][0],
                     row_index / 400 * 2 ** layer_index + layer_offsets[layer_index][1]
-                ) * 0.5 + 0.5
+                ) * 0.5 + 0.5)
             }
-            value = value / layer_number
+            max_value = Math.max(max_value, value)
             row.push(value)
         }
         values.push(row)
+    }
+
+    for (let row_index = 0; row_index < dimensions[1]; row_index++) {
+        for (let col_index = 0; col_index < dimensions[0]; col_index++) {
+            values[row_index][col_index]  /= max_value
+        }
     }
 
     return values
